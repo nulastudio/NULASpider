1f7c024966b76f6ae75e244fa73a8b890346cc18
LiesAuer <liesauer@liesauer.net>
2018-12-01 01:03:43 +0800

    Aria2Downloader->addUri()方法已完成

--- NULASpider.PHP/src/Networking/Rpc/Aria2Downloader.php
+++ NULASpider.PHP/src/Networking/Rpc/Aria2Downloader.php
@@ -14,12 +14,32 @@ class Aria2Downloader
         $this->aria2 = new SimpleAria2JsonRpcClient($url, $token);
     }
 
-    public function addUri(string $url, string $savePath = '', array $options = [])
+    /**
+     * 投递一个或多个下载链接至aria2，支持HTTP/HTTPS/FTP/SFTP/Magnet链接，当投递Magnet链接时，一次只能投递一个链接
+     * @param  mixed  $url                                                       投递链接
+     * @param  string $savePath                                                  保存位置，当以“/”结尾时，将当前文件保存到指定目录，否则保存到指定的文件名
+     * @param  array  $options                                                   额外参数，参见aria2文档
+     * @param  int    $position                                                  插入位置，当大于队列长度或未指定时插入到队列尾部
+     * @return mixed  成功返回最新下载链接的GID，否则返回false
+     */
+    public function addUri($url, string $savePath = '', array $options = [], int $position = null)
     {
         if ($savePath) {
-            $options['dir'] = $savePath;
+            $savePath = str_replace('\\', '/', $savePath);
+            if (substr($savePath, -1) === '/') {
+                $options['dir'] = substr($savePath, 0, -1);
+            } else {
+                $options['out'] = $savePath;
+            }
         }
-        $response = json_decode($this->aria2->addUri($url, $options ?: '{}'), true);
+        $params = [
+            is_array($url) ? $url : [$url],
+            $options ?: '{}',
+        ];
+        if ($position !== null) {
+            $params[] = $position;
+        }
+        $response = json_decode($this->aria2->addUri(...$params), true);
         return $response['result'] ?? false;
     }
 }
