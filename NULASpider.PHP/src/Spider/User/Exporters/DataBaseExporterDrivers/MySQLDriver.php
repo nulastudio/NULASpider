c5cf6be1a0b3a024b99c9c869823f026cbcb74ed
50percent <fifty.percent@hotmail.com>
2018-12-07 16:33:52 +0800

    修复没有use语句的问题

--- NULASpider.PHP/src/Spider/User/Exporters/DataBaseExporterDrivers/MySQLDriver.php
+++ NULASpider.PHP/src/Spider/User/Exporters/DataBaseExporterDrivers/MySQLDriver.php
@@ -2,6 +2,8 @@
 
 namespace User\Exporters\DataBaseExporterDrivers;
 
+use User\Exporters\DataBaseExporterDrivers\AbstructDriver;
+
 class MySQLDriver extends AbstructDriver
 {
     public function __construct(array $config = [])
