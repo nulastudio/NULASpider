54d35f74d5ec120a8c6ec516e2dd51608629e12f
50percent <fifty.percent@hotmail.com>
2018-12-07 17:20:01 +0800

    emmm，语法还是有错

--- NULASpider.PHP/src/Spider/User/Plugins/RandomUserAgent.php
+++ NULASpider.PHP/src/Spider/User/Plugins/RandomUserAgent.php
@@ -9,10 +9,8 @@ class RandomUserAgent implements PluginContract
 {
     public static function install($application, array $userUAs = [])
     {
-        $application->hooks['beforeRequest'][] = [
-            function ($spider, $request) use ($userUAs) {
-                $request->setHeader('User-Agent', $userUAs ? array_rand($userUAs) : UserAgent::random());
-            },
-        ];
+        $application->hooks['beforeRequest'][] = function ($spider, $request) use ($userUAs) {
+            $request->setHeader('User-Agent', $userUAs ? array_rand($userUAs) : UserAgent::random());
+        };
     }
 }
