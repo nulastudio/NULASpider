04adb4e1f248552d8b5b8b99823613af3efe5480
50percent <fifty.percent@hotmail.com>
2018-12-07 16:50:15 +0800

    Hook不再支持+=语法，改成新语法

--- NULASpider.PHP/src/Spider/User/Plugins/RandomUserAgent.php
+++ NULASpider.PHP/src/Spider/User/Plugins/RandomUserAgent.php
@@ -2,14 +2,14 @@
 
 namespace User\Plugins;
 
-use nulastudio\Spider\Contracts\PluginContract;
 use nulastudio\Networking\Http\UserAgent;
+use nulastudio\Spider\Contracts\PluginContract;
 
 class RandomUserAgent implements PluginContract
 {
     public static function install($application, array $userUAs = [])
     {
-        $application->hooks['beforeRequest'] += [
+        $application->hooks['beforeRequest'][] = [
             function ($spider, $request) use ($userUAs) {
                 $request->setHeader('User-Agent', $userUAs ? array_rand($userUAs) : UserAgent::random());
             },
