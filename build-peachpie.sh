addbd374593d20da9dc011cba4b03408cdea6cf9
L!εsAμεr <liesauer@liesauer.net>
2019-07-01 19:37:02 +0800

    Multi-Target Release

--- build-peachpie.sh
+++ build-peachpie.sh
@@ -6,19 +6,20 @@ if [[ $2 ]]; then
     Configuration=$2
 fi
 
-dotnet build ../peachpie/src/Peachpie.NET.Sdk/ /p:Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.Runtime/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.Library/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.Library.Graphics/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.Library.Network/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.Library.XmlDom/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.Library.Scripting/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.Library.MsSql/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.Library.MySql/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/PDO/Peachpie.Library.PDO/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/PDO/Peachpie.Library.PDO.MySQL/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/PDO/Peachpie.Library.PDO.SqlSrv/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.App/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.CodeAnalysis/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.AspNetCore.Web/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
-dotnet build ../peachpie/src/Peachpie.AspNetCore.Mvc/ /p:TargetFrameworks=netcoreapp2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.NET.Sdk/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.Runtime/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.Library/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.Library.Graphics/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.Library.Network/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.Library.XmlDom/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.Library.Scripting/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.Library.MsSql/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.Library.MySql/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/PDO/Peachpie.Library.PDO/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/PDO/Peachpie.Library.PDO.MySQL/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/PDO/Peachpie.Library.PDO.Sqlite/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/PDO/Peachpie.Library.PDO.SqlSrv/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.App/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.CodeAnalysis/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.AspNetCore.Web/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
+dotnet build ../peachpie/src/Peachpie.AspNetCore.Mvc/ /p:TargetFrameworks=netstandard2.0,Version=$1,Configuration=${Configuration}
