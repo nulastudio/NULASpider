ab77ed3fd39f62f0224d608e20db80acf928dd87
L!εsAμεr <liesauer@liesauer.net>
2019-07-01 12:01:54 +0800

    HackerNews clean up

--- NULASpider.PHP/User/spiders/HackerNews.php
+++ NULASpider.PHP/User/spiders/HackerNews.php
@@ -29,36 +29,21 @@ $config = [
         '#https://news.ycombinator.com/item\?id=\d+#',
     ],
     'fields'              => [
-        'title' => [
-            'type' => 'css',
-            'selector' => '.title>a@innerText',
-        ],
+        'title'    => 'css://.title>a@innerText',
         'comments' => [
-            'type' => 'css',
+            'type'     => 'css',
             'selector' => '.comment-tree>tr',
-            'fields'=>[
-                'ind'=>[
-                    'type'=>'css',
-                    'selector'=> '.ind>img@width',
-                ],
-                'author' => [
-                    'type' => 'css',
-                    'selector' => '.hnuser@innerText',
-                ],
-                'time' => [
-                    'type' => 'css',
-                    'selector' => '.age>a@innerText',
-                ],
-                'comment' => [
-                    'type' => 'css',
-                    'selector' => '.comment@innerText',
-                ],
+            'fields'   => [
+                'ind'     => 'css://.ind>img@width',
+                'author'  => 'css://.hnuser@innerText',
+                'time'    => 'css://.age>a@innerText',
+                'comment' => 'css://.comment@innerText',
             ],
             'repeated' => true,
         ],
     ],
     'export'              => [
-        'type'=>'print',
+        'type' => 'print',
     ],
 ];
 
@@ -70,56 +55,12 @@ $spider = new Spider($config);
 # 回调钩子
 # ====================
 
-// $spider->on_start = function ($spider) {
-//     Request::getDefaultHeader()->setHeaders([
-//         'User-Agent' => UserAgent::USER_AGENTS['WIN10_X64_EDGE'],
-//     ]);
-//     // fiddler proxy
-//     // Request::getDefaultOption()->proxy = 'http://127.0.0.1:8888';
-// };
-
-// 常规回调
-// $spider->on_exit = function ($spider, $exit_code) {};
-// $spider->on_request = function ($spider, $request, $response) {};
-// $spider->on_status_code = function ($spider, $status_code, $request, $response) {};
-// $spider->on_process = function ($spider, $url, $request, $response) {};
-// $spider->on_scan_url = function ($spider, $url, $request, $response) {};
-// $spider->on_list_url = function ($spider, $url, $request, $response) {};
-// $spider->on_content_url = function ($spider, $url, $request, $response) {};
-// $spider->on_fetch_field = function ($spider, $name, $field) {};
-// $spider->on_fetch_page = function ($spider, $fields, $request, $response) {};
-// $spider->on_export = function ($spider, $export, $fields, $request, $response) {};
-
-// 异常回调
-// $spider->on_error = function ($spider, $errno, $errstr, $errfile, $errline, $errcontext) {};
-// $spider->on_exception = function ($spider, $ex) {};
-
-// 高级回调
-// $spider->requestOverride = function ($spider, $request) {};
-// $spider->findUrlsOverride = function ($spider, $content, $request, $response) {};
-// $spider->filterUrls = function ($spider, $urls) {};
-// $spider->encodingHandler = function ($spider, $response) {};
-
-// 系统钩子
-// $spider->hooks['beforeRequest'][] = function ($spider, $request) {};
-// $spider->hooks['beforeExit'][] = function ($spider, $exit_code) {};
-
-# ====================
-
 # ====================
 # 插件加载
 # ====================
 
 $spider->use(User\Plugins\Buff::class);
-
-// $spider->use(User\Plugins\CSVExporter::class);
-// $spider->use(User\Plugins\ExcelExporter::class);
-// $spider->use(User\Plugins\JsonExporter::class);
-// $spider->use(User\Plugins\PrintOutExporter::class);
-
-// $spider->use(User\Plugins\Pipeline::class);
-// $spider->use(User\Plugins\ProxyPool::class);
-// $spider->use(User\Plugins\RandomUserAgent::class);
+$spider->use(User\Plugins\PrintOutExporter::class);
 
 # ====================
 
