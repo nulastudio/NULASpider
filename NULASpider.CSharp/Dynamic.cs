8c40dcf8d3f1bcb07bd3eb0bfcba7c7429a4ae31
L!εsAμεr <liesauer@liesauer.net>
2019-07-01 18:05:11 +0800

    动态加载程序集

--- NULASpider.CSharp/Dynamic.cs
+++ NULASpider.CSharp/Dynamic.cs
@@ -15,10 +15,21 @@ public static class Dynamic
             EmitDebugInformation = false,
             IsSubmission = false,
             AdditionalReferences = new string[] {
-                typeof(nulastudio.Collections.RedisQueue).Assembly.Location,
-                typeof(nulastudio.Collections.RedisUniqueQueue).Assembly.Location,
-                typeof(nulastudio.Collections.ConcurrentRedisQueue).Assembly.Location,
-                typeof(nulastudio.Collections.ConcurrentRedisUniqueQueue).Assembly.Location,
+                typeof(System.Object).Assembly.Location,                                  // mscorlib
+                typeof(Pchp.Core.Context).Assembly.Location,                              // Peachpie.Runtime
+                typeof(Pchp.Library.Strings).Assembly.Location,                           // Peachpie.Library
+                typeof(Peachpie.Library.XmlDom.DOMDocument).Assembly.Location,            // Peachpie.Library.XmlDom
+                typeof(Peachpie.Library.Scripting.PhpFunctions).Assembly.Location,        // Peachpie.Library.Scripting
+                typeof(Peachpie.Library.Network.CURLFunctions).Assembly.Location,         // cURL
+                typeof(Peachpie.Library.Graphics.PhpGd2).Assembly.Location,               // GD2
+                typeof(Peachpie.Library.MySql.MySql).Assembly.Location,                   // MySql
+                typeof(Peachpie.Library.MsSql.MsSql).Assembly.Location,                   // MsSql
+                typeof(Peachpie.Library.PDO.PDO).Assembly.Location,                       // PDO
+                typeof(Peachpie.Library.PDO.MySQL.PDOMySQLDriver).Assembly.Location,      // PDO.MySQL
+                typeof(Peachpie.Library.PDO.Sqlite.PDOSqliteDriver).Assembly.Location,    // PDO.Sqlite
+                typeof(Peachpie.Library.PDO.SqlSrv.PDOSqlServerDriver).Assembly.Location, // PDO.SqlServer
+                typeof(nulastudio.Spider.Application).Assembly.Location,                  // NULASpider.CSharp
+                System.Reflection.Assembly.GetEntryAssembly().Location,                   // NULASpider.PHP
             },
         }, File.ReadAllText(scriptFile));
 
