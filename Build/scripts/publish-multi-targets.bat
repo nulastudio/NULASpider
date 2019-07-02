b9a627ff3c330044302ecd3deb307c74adea0acd
L!εsAμεr <liesauer@liesauer.net>
2019-07-02 23:48:53 +0800

    Optimize Build Stuff

--- Build/scripts/publish-multi-targets.bat
+++ Build/scripts/publish-multi-targets.bat
@@ -3,15 +3,21 @@
 for /f "delims=" %%i in ('git rev-list --tags --max-count=1') do set latestCommit=%%i
 for /f "delims=" %%i in ('git describe --tags %latestCommit%') do set latestTag=%%i
 
-set workdir=%~dp0..\
+set workdir=%~dp0..\Release\
+
+if exist %workdir% (
+    rd /s/q %workdir%
+)
 
 for %%i in (win-x64 win-x86 linux-x64 osx-x64) do (
     echo\
     echo publishing target %%i
     echo\
-    dotnet publish -c=Release -r=%%i -o=%workdir%..\Release\%%i\ %workdir%..\..\NULASpider.PHP\NULASpider.PHP.msbuildproj
-    cd %workdir%..\Release\%%i
-    %workdir%..\tools\windows\7za\x86\7za.exe -r a %workdir%..\Release\%%i-%latestTag%.zip .
+    dotnet publish -c=Release -r=%%i -o=%workdir%%%i\ %workdir%..\..\NULASpider.PHP\NULASpider.PHP.msbuildproj
+    if exist %workdir%%%i (
+        cd %workdir%%%i
+        %workdir%..\tools\windows\7za\x86\7za.exe -r a %workdir%%%i-%latestTag%.zip .
+    )
 )
 
 echo\
