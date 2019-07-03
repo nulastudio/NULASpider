09f2b9a508c621ae1d90324c0f9ff3d59586e090
50% <fifty.percent@hotmail.com>
2019-07-03 09:27:55 +0800

    Optimize Build Stuff

--- Build/scripts/publish-multi-targets.sh
+++ Build/scripts/publish-multi-targets.sh
@@ -4,13 +4,19 @@ workdir=$(cd $(dirname $0); pwd)
 targets=("win-x64" "win-x86" "linux-x64" "osx-x64");
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-rm -rf ${workdir}/../Release/
+workdir=${workdir}/../Release
+
+if [ -d ${workdir} ];then
+    rm -rf ${workdir}
+fi
 
 for target in ${targets[@]}; do
     echo "\npublishing target ${target}\n"
-    dotnet publish -c=Release -r=${target} -o=${workdir}/../Release/${target}/ ${workdir}/../../NULASpider.PHP/NULASpider.PHP.msbuildproj
-    cd ${workdir}/../Release/${target}/
-    zip -r ${workdir}/../Release/${target}-${latestTag}.zip .
+    dotnet publish -c=Release -r=${target} -o=${workdir}/${target}/ ${workdir}/../../NULASpider.PHP/NULASpider.PHP.msbuildproj
+    if [ -d ${workdir}/${target} ];then
+        cd ${workdir}/${target}
+        zip -r ${workdir}/${target}-${latestTag}.zip .
+    fi
 done
 
 echo "\nall done!\n"
